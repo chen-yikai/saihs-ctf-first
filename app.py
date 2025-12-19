7d6ca53ee20dcc6e9eb95f90059573f5e474b407
elias <eliaschen.dev@gmail.com>
2025-12-19 15:22:37 +0800

    remove

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def claim():
         return jsonify({'ok': False, 'error': 'not enough'}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='saihs-ctf-first-try.zeabur.app', port=PORT, debug=False)
